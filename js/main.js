782da58b3746e04b71ff616b0f3f50731acea7f4
Spyros Lefkaditis <slefkaditis@gmail.com>
2015-03-04 12:43:58 +0000

    Fixed JS on checkout
    Signed-off-by: Spyros Lefkaditis <slefkaditis@gmail.com>

--- js/main.js
+++ js/main.js
@@ -452,7 +452,27 @@
 						$('.checkout-progress li').removeClass('current').filter('[data-id='+id+']').addClass('current');
 					});
 
-					
+					$('#payment').on( 'click', '.payment_methods input.input-radio', function() {
+						if ( $( '.payment_methods input.input-radio' ).length > 1 ) {
+							var target_payment_box = $( 'div.payment_box.' + $( this ).attr( 'ID' ) );
+
+							if ( $( this ).is( ':checked' ) && ! target_payment_box.is( ':visible' ) ) {
+								$( 'div.payment_box' ).filter( ':visible' ).slideUp( 250 );
+
+								if ( $( this ).is( ':checked' ) ) {
+									$( 'div.payment_box.' + $( this ).attr( 'ID' ) ).slideDown( 250 );
+								}
+							}
+						} else {
+							$( 'div.payment_box' ).show();
+						}
+
+						if ( $( this ).data( 'order_button_text' ) ) {
+							$( '#place_order' ).val( $( this ).data( 'order_button_text' ) );
+						} else {
+							$( '#place_order' ).val( $( '#place_order' ).data( 'value' ) );
+						}
+					})
 				}	
 			}
 		},
